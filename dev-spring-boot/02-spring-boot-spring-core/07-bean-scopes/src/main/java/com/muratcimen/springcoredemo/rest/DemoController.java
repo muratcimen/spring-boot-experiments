fa3459c9fdef65e3fcad493ebdd9382491627881
Murat ÇİMEN <cimnmurat@gmail.com>
2023-11-01 22:52:40 +0300

    MCA_SP_BEAN_SCOPES

---
+++ dev-spring-boot/02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/muratcimen/springcoredemo/rest/DemoController.java
@@ -0,0 +1,33 @@
+package com.muratcimen.springcoredemo.rest;
+
+import com.muratcimen.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //define a private field for dependency
+    private Coach myCoach;
+    private Coach anotherCoach;
+
+    @Autowired
+    public DemoController (@Qualifier("cricketCoach") Coach theCoach,
+                           @Qualifier("cricketCoach") Coach theAnotherCoach) {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        myCoach=theCoach;
+        anotherCoach = theAnotherCoach;
+    }
+    @GetMapping("/getDailyWorkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
+    }
+
+}
